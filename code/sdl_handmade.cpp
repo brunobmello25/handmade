7a82f2acf5b6ef8527ad47ee068d1039653fc99b
Bruno Mello <bruno.barros.mello@gmail.com>
2025-07-02 23:58:24 -0300

    drawing a window!

--- code/sdl_handmade.cpp
+++ code/sdl_handmade.cpp
@@ -1,7 +1,61 @@
+#include "SDL_events.h"
+#include "SDL_video.h"
 #include <SDL.h>
+#include <cstdio>
 
-int main(int argc, char *argv[]) {
-  SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "Handmade Hero",
-                           "This is Handmade Hero", 0);
+bool HandleEvent(SDL_Event *Event)
+{
+  bool ShouldQuit = false;
+
+  switch (Event->type)
+  {
+    case SDL_QUIT:
+    {
+      printf("SDL_QUIT\n");
+      ShouldQuit = true;
+    }
+    break;
+    case SDL_WINDOWEVENT:
+    {
+      switch (Event->window.event)
+      {
+        case SDL_WINDOWEVENT_RESIZED:
+        {
+          printf("SDL_WINDOWEVENT_RESIZEd: %d x %d\n", Event->window.data1,
+                 Event->window.data2);
+        }
+        break;
+      }
+    }
+    break;
+  }
+
+  return ShouldQuit;
+}
+
+int main(int argc, char *argv[])
+{
+
+  if (SDL_Init(SDL_INIT_VIDEO) != 0)
+  {
+    // TODO: Handle error
+  }
+
+  SDL_Window *Window;
+  Window =
+      SDL_CreateWindow("Handmade Hero", SDL_WINDOWPOS_UNDEFINED,
+                       SDL_WINDOWPOS_UNDEFINED, 640, 480, SDL_WINDOW_RESIZABLE);
+
+  while (true)
+  {
+    SDL_Event event;
+    SDL_WaitEvent(&event);
+
+    if (HandleEvent(&event))
+    {
+      break;
+    }
+  }
+
   return (0);
 }
