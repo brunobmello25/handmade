0e17d6e0fa3932cbe86286bed21dcbd0d1d2bf60
Bruno Mello <bruno.barros.mello@gmail.com>
2025-09-22 14:56:14 -0300

    begin raw x11 implementation

---
+++ code/linux_handmade.cpp
@@ -0,0 +1,71 @@
+#include <X11/Xlib.h>
+#include <X11/Xos.h>
+#include <X11/Xutil.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+Display *dis;
+int screen;
+Window win;
+GC gc;
+
+void init_x()
+{
+	/* get the colors black and white (see section for details) */
+	unsigned long black, white;
+
+	/* use the information from the environment variable DISPLAY
+	   to create the X connection:
+	*/
+	dis = XOpenDisplay((char *)0);
+	screen = DefaultScreen(dis);
+	printf("screen is %d\n", screen);
+	black = BlackPixel(dis, screen),	 /* get color black */
+		white = WhitePixel(dis, screen); /* get color white */
+
+	/* once the display is initialized, create the window.
+	   This window will be have be 200 pixels across and 300 down.
+	   It will have the foreground white and background black
+	*/
+	win = XCreateSimpleWindow(dis, DefaultRootWindow(dis), 0, 0, 200, 300, 5,
+							  white, black);
+
+	/* here is where some properties of the window can be set.
+	   The third and fourth items indicate the name which appears
+	   at the top of the window and the name of the minimized window
+	   respectively.
+	*/
+	XSetStandardProperties(dis, win, "My Window", "HI!", None, NULL, 0, NULL);
+
+	/* this routine determines which types of input are allowed in
+	   the input.  see the appropriate section for details...
+	*/
+	XSelectInput(dis, win, ExposureMask | ButtonPressMask | KeyPressMask);
+
+	/* create the Graphics Context */
+	gc = XCreateGC(dis, win, 0, 0);
+
+	/* here is another routine to set the foreground and background
+	   colors _currently_ in use in the window.
+	*/
+	XSetBackground(dis, gc, white);
+	XSetForeground(dis, gc, black);
+
+	/* clear the window and bring it on top of the other windows */
+	XClearWindow(dis, win);
+	XMapRaised(dis, win);
+};
+
+int main(void)
+{
+	init_x();
+
+	char tmp[512];
+	while (True)
+	{
+		scanf("%s", tmp);
+		printf("%s", tmp);
+	}
+
+	return 0;
+}
