8d450985d21ae83ed6ec2091f7173640f2addaa8
Bruno Mello <bruno.barros.mello@gmail.com>
2025-10-05 19:50:37 -0300

    move some platform independent defines and typedefs to handmade.h

--- code/sdl_handmade.cpp
+++ code/sdl_handmade.cpp
@@ -1,24 +1,6 @@
-#include <stdint.h>
 #define internal static
 #define local_persist static
 #define global_variable static
-
-#define Pi32 3.14159265359f
-
-typedef int8_t int8;
-typedef int16_t int16;
-typedef int32_t int32;
-typedef int64_t int64;
-
-typedef uint8_t uint8;
-typedef uint16_t uint16;
-typedef uint32_t uint32;
-typedef uint64_t uint64;
-
-typedef int32 bool32;
-
-typedef float real32;
-typedef double real64;
 
 // TODO: Implement sine ourselved
 #include <math.h>
