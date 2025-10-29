1c3f3b9aa855e9f629abc3291cdb490a77358365
Bruno Mello <bruno.barros.mello@gmail.com>
2025-10-29 19:11:51 -0300

    opening a window

--- code/sdl3_handmade.cpp
+++ code/sdl3_handmade.cpp
@@ -1,48 +1,86 @@
 #include <SDL3/SDL.h>
-#include <SDL3/SDL_events.h>
 #include <stdlib.h>
 
 #define global_variable static
 
-global_variable int *globalBackbuffer;
+struct SDLBackbuffer
+{
+	int width;
+	int height;
+	void *memory;
+	SDL_Texture *texture;
+};
+
+global_variable SDLBackbuffer globalBackbuffer;
 global_variable bool globalRunning;
-global_variable SDL_Window *globalSDLWindow;
 
-const int WINDOW_WIDTH = 1920 / 2;
-const int WINDOW_HEIGHT = 1080 / 2;
-
-void processSDLEvents()
+bool processSDLEvents()
 {
 	SDL_Event event;
 
 	while (SDL_PollEvent(&event))
 	{
+		if (event.type == SDL_EVENT_QUIT)
+		{
+			return false;
+		}
+		if (event.type == SDL_EVENT_KEY_UP && event.key.key == SDLK_ESCAPE)
+		{
+			return false;
+		}
 	}
+	return true;
+}
+
+// function that gets called everytime the window size changes, and also
+// at first time, in order to allocate the backbuffer with proper dimensions
+void resizeSDLBackbuffer(SDLBackbuffer *backbuffer, SDL_Renderer *renderer,
+						 int width, int height)
+{
+	int bytesPerPixel = 4;
+
+	if (backbuffer->memory)
+		free(backbuffer->memory);
+
+	if (backbuffer->texture)
+		SDL_DestroyTexture(backbuffer->texture);
+
+	backbuffer->width = width;
+	backbuffer->height = height;
+	backbuffer->memory = malloc(width * height * bytesPerPixel);
+	backbuffer->texture =
+		SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
+						  SDL_TEXTUREACCESS_STREAMING, width, height);
 }
 
 int main(void)
 {
+	int initialWidth = 1920 / 2;
+	int initialHeight = 1080 / 2;
+
 	if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_EVENTS))
-	{
 		return -1;
-	}
-
-	globalBackbuffer = (int *)malloc(WINDOW_WIDTH * WINDOW_HEIGHT);
-	globalSDLWindow = SDL_CreateWindow("Handmade Hero", WINDOW_WIDTH,
-									   WINDOW_HEIGHT, SDL_WINDOW_RESIZABLE);
-
-	if (!globalBackbuffer || !globalSDLWindow)
-	{
+	SDL_Window *window = SDL_CreateWindow("Handmade Hero", initialWidth,
+										  initialHeight, SDL_WINDOW_RESIZABLE);
+	if (!window) // TODO(bruno): proper error handling
 		return -1;
-	}
 
+	SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
+	if (!renderer) // TODO(bruno): proper error handling
+		return -1;
+
+	globalBackbuffer = {};
+	resizeSDLBackbuffer(&globalBackbuffer, renderer, initialWidth,
+						initialHeight);
 	globalRunning = true;
 
 	while (globalRunning)
 	{
+		globalRunning = processSDLEvents();
 	}
 
-	SDL_DestroyWindow(globalSDLWindow);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
 	SDL_Quit();
 
 	return 0;
