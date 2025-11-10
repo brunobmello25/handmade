d856ee6b94c2fa153fef27f48133b4d7c2757419
Bruno Mello <bruno.barros.mello@gmail.com>
2025-11-09 23:46:22 -0300

    autobuild cpp code on save

--- .nvimrc/init.lua
+++ .nvimrc/init.lua
@@ -2,3 +2,11 @@ vim.keymap.set("n", "<M-m>", function()
 	-- vim.cmd("!bin/build && target/handmade")
 	vim.cmd("!bin/build")
 end, {})
+
+local build_augroup = vim.api.nvim_create_augroup("build", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+	group = build_augroup,
+	callback = function()
+		vim.cmd("silent !bin/build > /dev/null 2>&1")
+	end,
+})
